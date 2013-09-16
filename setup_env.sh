4fc6f9299d25fe4d185972807747f9f2fb224f94
Michael Souza <pongosapien@knomsa.in>
2013-09-16 03:13:50 -0700

    Added a notebook to html script.

--- setup_env.sh
+++ setup_env.sh
@@ -3,9 +3,11 @@ git clone git://github.com/yyuu/pyenv-virtualenv.git ./.pyenv/plugins/pyenv-virt
 export PYENV_ROOT="`pwd`/.pyenv"
 export PATH="`pwd`/.pyenv/bin:$PATH"
 eval "$(pyenv init -)"
+export CONFIGURE_OPTS="--enable-shared"
 pyenv install 2.7.5
 pyenv global 2.7.5
 pyenv rehash
 pyenv virtualenv 2.7.5 project_env
 pyenv shell project_env
+pip install --upgrade pip
 cat requirements.txt | xargs pip install
